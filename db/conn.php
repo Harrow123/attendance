87945fd39dfaae9d3838127e2652ad728b1be2d9
Harrow123 <92832305+Harrow123@users.noreply.github.com>
2021-12-31 19:45:37 -0500

    Update conn.php

--- db/conn.php
+++ db/conn.php
@@ -1,17 +1,17 @@
 <?php
     //Development Connection
-    $host = '127.0.0.1';
-    $db = 'attendance_db';
-    $user = 'root';
-    $pass = '';
-    $charset = 'utf8mb4';
+    //$host = '127.0.0.1';
+    //$db = 'attendance_db';
+    //$user = 'root';
+    //$pass = '';
+    //$charset = 'utf8mb4';
 
     //Remote Database Connection
-    //$host = 'remotemysql.com';
-    //$db = 'fky2nxaKI3';
-    //$user = 'fky2nxaKI3';
-    //$pass = 'ftc6aLINJP';
-    //$charset = 'utf8mb4';
+    $host = 'remotemysql.com';
+    $db = 'fky2nxaKI3';
+    $user = 'fky2nxaKI3';
+    $pass = 'ftc6aLINJP';
+    $charset = 'utf8mb4';
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
